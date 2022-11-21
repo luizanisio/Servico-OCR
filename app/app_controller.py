dd0535e6f2344d53d984c4463fd595191d59627f
Luiz Anísio Vieira <luizanisio@gmail.com>
2022-11-20 22:19:18 -0300

    visual para acompanhamento da ocerização de PDF

--- app/app_controller.py
+++ app/app_controller.py
@@ -3,16 +3,18 @@ from tempfile import TemporaryDirectory
 from util_pdf_ocr import imagens_pdf
 from util_ocr import analise_imagens_ocr
 from util_html import aimg_2_html
-from util import hash_file, listar_arquivos, limpar_temporarios
+from util import Util
+from util_processar_pasta import ProcessarOcrThread
 import os
 import json
+import shutil
 
 class Controller():
 
     def aplicar_ocr_arquivo(self, arquivo):
         return {}
 
-    def request_file_pdf(self, request_file):
+    def request_file_send(self, request_file):
         if type(request_file) is str:
             return None
         # arquivo pdf, testa o conteúdo
@@ -45,17 +47,74 @@ class Controller():
     def alerta(self, mensagem):
         return f'<div class="p-3 mb-2 bg-warning text-dark">{mensagem}</div>'
 
-    def ocerizar_imagens_para_html(self, request_file, ignorar_cache = False, cabecalho = True, estampas = True, citacoes = True):
+    def ocerizar_arquivo_recebido(self, request_file, ignorar_cache = False, cabecalho = True, estampas = True, citacoes = True, saida_pdf = False):
+        if not saida_pdf:
+            return self.ocerizar_arquivo_para_html(request_file = request_file,
+                                                   ignorar_cache = ignorar_cache, 
+                                                   cabecalho = cabecalho, 
+                                                   estampas = estampas, 
+                                                   citacoes = citacoes)
+        else:
+            return self.ocerizar_arquivo_para_pdf(request_file = request_file,
+                                                   ignorar_cache = ignorar_cache)
+
+
+    def ocerizar_arquivo_para_pdf(self, request_file, ignorar_cache = False):
+        ''' retorna o hash para criar um link de download e o status da ocerização 
+            {"erro":, "hash": ..., "inicio": ...., "fim": ...., "tempo": }
+            '''
         if not request_file:
-            return {}, ''
+            return {}
         with TemporaryDirectory() as tempdir:
              arquivo_entrada = self.__arquivo_analise__(request_file, tempdir)
              if not os.path.isfile(arquivo_entrada):
-                return self.alerta('Arquivo não encontrado')
-             hash_arquivo = hash_file(arquivo_entrada)
+                a = os.path.split(arquivo_entrada)[1]
+                return {'erro': f'Arquivo não encontrado "{a}"' }
+             hash_arquivo = Util.hash_file(arquivo_entrada)
+             destino_entrada = os.path.join(ProcessarOcrThread.servico().entrada, f'{hash_arquivo}.pdf')
+             destino_json = os.path.join(ProcessarOcrThread.servico().entrada, f'{hash_arquivo}.json')
+             json_status_inicial = {'status': 'incluído na pasta de entrada', 
+                                    'nome_real': os.path.split(arquivo_entrada)[1],
+                                    'inicio': Util.data_hora_str(),
+                                    'tamanho_inicial' : round(os.path.getsize(arquivo_entrada)/1024,2)}
+             if ignorar_cache:
+                status_arquivo = {}
+             else:   
+                status_arquivo = ProcessarOcrThread.servico().status_arquivo(hash_arquivo)
+             print(f'Processar PDF "{arquivo_entrada}" >> "{destino_entrada}"')
+             if not any(status_arquivo):
+                if arquivo_entrada.find('_cache.') > 0:
+                    shutil.move(arquivo_entrada, destino_entrada)
+                else:
+                    shutil.copy(arquivo_entrada, destino_entrada)
+                status_arquivo.update(json_status_inicial)
+                Util.gravar_json(destino_json, json_status_inicial)
+                status_arquivo = ProcessarOcrThread.servico().status_arquivo(hash_arquivo)
+                
+             if 'status' in status_arquivo:
+                status_arquivo['tipo_folha'] = 'PDF'
+                return status_arquivo
+        return {'erro' : 'Não foi possívle identificar o status do arquivo enviado'}
+
+    def status_por_id(self, id_arquivo):
+        return ProcessarOcrThread.servico().status_arquivo(id_arquivo)
+
+    def ocerizar_arquivo_para_html(self, request_file, ignorar_cache = False, cabecalho = True, estampas = True, citacoes = True):
+        ''' retorna o conteúdo da extração em html e o tipo da folha reconhecido
+            pode retornar uma mensagem de erro ou o hash para acompanhamento posterior
+            {"html": ..., "tipo_folha": ..., "erro":, "hash": ..., }
+            '''
+        if not request_file:
+            return {}
+        with TemporaryDirectory() as tempdir:
+             arquivo_entrada = self.__arquivo_analise__(request_file, tempdir)
+             if not os.path.isfile(arquivo_entrada):
+                a = os.path.split(arquivo_entrada)[1]
+                return {'erro': f'Arquivo não encontrado "{a}"' }
+             hash_arquivo = Util.hash_file(arquivo_entrada)
              print(f'Extraindo dados do arquivo: "{arquivo_entrada}"  hash: {hash_arquivo}')
 
-            # nome do arquivo json de análise
+             # nome do arquivo json de análise
              pasta = './temp/'
              #arquivo_json = os.path.splitext( os.path.split(arquivo_entrada)[1])[0]
              #arquivo_json = 
@@ -63,18 +122,15 @@ class Controller():
              print('Procurando cache no arquivo: ', arquivo_json, end='')
              # análise em cache
              if (not ignorar_cache) and os.path.isfile(arquivo_json):
-                with open(arquivo_json, 'r') as f:
-                        dados = f.read()
-                if len(dados)>10 and dados.find('{')>=0 and dados[-5:].find('}')>=0:
+                dados = Util.ler_json(arquivo_json)
+                if any(dados):
                     print(' ... cache encontrado _o/')
-                    dados = json.loads(dados)
                     aimg = analise_imagens_ocr(dados)
                     dados = aimg.dados()
-                    with open(arquivo_json, 'w') as f:
-                        f.write(json.dumps(dados, indent = 2))
+                    Util.gravar_json(arquivo_json, dados)
                     dados = self.filtrar_dados(dados = dados, cabecalho=cabecalho, 
                                                estampas=estampas, citacoes = citacoes)
-                    return aimg_2_html(dados), self.tipo_folha(dados)
+                    return {'html': aimg_2_html(dados), 'tipo_folha' : self.tipo_folha(dados) }
              # nova análise
              if ignorar_cache:
                 print(' ... cache ignorado _o/')
@@ -87,11 +143,10 @@ class Controller():
                 imagens = arquivo_entrada
              aimg = analise_imagens_ocr(imagens)
              dados = aimg.dados()
-             with open(arquivo_json, 'w') as f:
-                 f.write(json.dumps(dados, indent = 2))
+             Util.gravar_json(arquivo_json, dados)
              dados = self.filtrar_dados(dados = dados, cabecalho=cabecalho, 
                                         estampas=estampas, citacoes = citacoes)
-        return aimg_2_html(dados), self.tipo_folha(dados)
+        return {'html': aimg_2_html(dados), 'tipo_folha' : self.tipo_folha(dados) }
 
     def filtrar_dados(self, dados, cabecalho, estampas, citacoes):
         if cabecalho and estampas and citacoes:
@@ -123,27 +178,34 @@ class Controller():
 
         # veio o nome de um arquivo de exemplo
         if request_file and type(request_file) is str:
-           arquivo_pdf = str(request_file)
-           arquivo_pdf = os.path.split(arquivo_pdf)[1]
-           return os.path.join('./exemplos/', arquivo_pdf)
+           nome_arquivo = str(request_file)
+           nome_arquivo = os.path.split(nome_arquivo)[1]
+           return os.path.join('./exemplos/', nome_arquivo)
 
         # veio o conteúdo do arquivo - copia ele para a pasta temporária
-        file_pdf = self.request_file_pdf(request_file)
+        file_pdf = self.request_file_send(request_file)
         if file_pdf:
-            arquivo_pdf = os.path.split(file_pdf.filename)[1]
-            arquivo_pdf = os.path.join(tempdir, f'cache_{arquivo_pdf}') 
-            file_pdf.save(arquivo_pdf)
-            return arquivo_pdf
+            nome_arquivo = os.path.split(file_pdf.filename)[1]
+            nome_arquivo = os.path.join(tempdir, f'cache_{nome_arquivo}') 
+            file_pdf.save(nome_arquivo)
+            return nome_arquivo
 
         return None
 
     def listar_exemplos(self):
-        exemplos_pdf = listar_arquivos(pasta = './exemplos/', extensao = 'pdf')
-        exemplos_png = listar_arquivos(pasta = './exemplos/', extensao = 'png')
-        exemplos_jpg = listar_arquivos(pasta = './exemplos/', extensao = 'jpg')
+        exemplos_pdf = Util.listar_arquivos(pasta = './exemplos/', extensao = 'pdf')
+        exemplos_png = Util.listar_arquivos(pasta = './exemplos/', extensao = 'png')
+        exemplos_jpg = Util.listar_arquivos(pasta = './exemplos/', extensao = 'jpg')
         exemplos = exemplos_pdf + exemplos_jpg + exemplos_png
         #print('Exemplos: ', exemplos)
         return [(os.path.split(nm)[1], nm) for nm in exemplos]
 
     def limpar_temporarios(self):
-        limpar_temporarios(5)
+        Util.limpar_temporarios(5)
+
+    def arquivo_saida(self, hash):
+        arquivo = os.path.join(ProcessarOcrThread.servico().saida, f'{hash}.pdf')
+        return arquivo if os.path.isfile(arquivo) else None
+
+    def status_arquivo_saida(self, hash):
+        return ProcessarOcrThread.servico().status_arquivo(hash)
--- app/servico.py
+++ app/servico.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from copy import deepcopy
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, send_file
 from flask import render_template_string, redirect, url_for #, Markup
 from markupsafe import Markup
 
@@ -15,6 +15,7 @@ from datetime import datetime
 ###################################################################
 # controller
 from app_controller import Controller
+from util import Util 
 
 app = Flask(__name__, template_folder='./templates')
 bootstrap  = Bootstrap4(app)
@@ -44,9 +45,25 @@ def get_health():
 # recebe um PDF ou imagem
 # retorna json com a análise do PDF
 @app.route(f'{PATH_API}analisar_arquivo', methods=['GET','POST'])
-def srv_analisar_regras():
+def srv_analisar_arquivo():
     dados = get_post(request)
-    return jsonify( {'retorno': 'teste - não imlementado'} )
+    return jsonify( {'retorno': 'uso simplificado de API - ainda não imlementado'} )
+
+###################################################################
+# recebe um hash e retorna o arquivo se estiver pronto
+@app.route(f'{PATH_API}download', methods=['GET','POST'])
+def srv_download_arquivo():
+    dados = get_post(request)
+    controller = Controller()
+    _id = dados.get('id','')
+    status = controller.status_arquivo_saida(_id)
+    if 'download' in status and 'pasta' in status and 'arquivo_pdf' in status:
+       arquivo_pdf = os.path.join(status.get('pasta',''), status.get('arquivo_pdf',''))
+       if os.path.isfile(arquivo_pdf):
+          nome = status.get('nome_real', os.path.split(arquivo_pdf)[1] )
+          print(f'Enviando arquivo: "{arquivo_pdf}" como "{nome}" ')
+          return send_file(arquivo_pdf, as_attachment=True, attachment_filename =nome)
+    return controller.alerta(f'Arquivo não encontrado para download id={_id}')
 
 #############################################################################
 #############################################################################
@@ -66,48 +83,68 @@ def frm_analisar_arquivo():
         com_cabecalho = 'CHECKED' if inicio or dados.get('com_cabecalho') in (True,'on','S',1) else ''
         com_estampas =  'CHECKED' if inicio or dados.get('com_estampas') in (True,'on','S',1)  else ''
         com_citacoes =  'CHECKED' if inicio or dados.get('com_citacoes') in (True,'on','S',1)  else ''
+        id_arquivo = dados.get('id')
+        saida_pdf =  'CHECKED' if dados.get('saida_pdf') in (True,'on','S',1)  else ''
         exemplo = str(dados.get('exemplo',''))
         exemplo = '' if exemplo.find(' -- exemplos')>=0 else exemplo
 
-        tem_arquivo_enviado = controller.request_file_pdf(request_file = request)
+        tem_arquivo_enviado = controller.request_file_send(request_file = request)
         _ini = datetime.now()
-        tipo_folha = ''
         try:
+            res = {'html' : ""}
+            # página inicial, sem envio de informações, roda a limpeza dos temporários
             if ('submit' not in dados):
-                html_ocr = '' 
                 controller.limpar_temporarios()
+            # tem um exemplo selecionado e não foi enviado arquivo
             elif exemplo and not tem_arquivo_enviado:
-                print('Ocerizando arquivo de exemplo: ', exemplo)
-                html_ocr, tipo_folha = controller.ocerizar_imagens_para_html(exemplo,
-                                                                    ignorar_cache = ignorar_cache, 
-                                                                    cabecalho = com_cabecalho, 
-                                                                    estampas = com_estampas,
-                                                                    citacoes = com_citacoes)
-            elif not tem_arquivo_enviado:
-                html_ocr = '' 
+                print(f'Ocerizando arquivo de exemplo: {exemplo}')
+                res = controller.ocerizar_arquivo_recebido(exemplo,
+                                                            ignorar_cache = ignorar_cache, 
+                                                            cabecalho = com_cabecalho, 
+                                                            estampas = com_estampas,
+                                                            citacoes = com_citacoes,
+                                                            saida_pdf = saida_pdf)
+            # foi enviado um id específico par acompanhar
+            elif id_arquivo and not tem_arquivo_enviado:
+                print(f'Localizando id: {id_arquivo}')
+                res = controller.status_por_id(id_arquivo)
+            # foi enviado um arquivo
             else:
                 print('Ocerizando arquivo recebido ...')
-                html_ocr, tipo_folha = controller.ocerizar_imagens_para_html(request, 
-                                                                    ignorar_cache = ignorar_cache, 
-                                                                    cabecalho = com_cabecalho, 
-                                                                    estampas = com_estampas,
-                                                                    citacoes = com_citacoes)
+                res = controller.ocerizar_arquivo_recebido(request, 
+                                                            ignorar_cache = ignorar_cache, 
+                                                            cabecalho = com_cabecalho, 
+                                                            estampas = com_estampas,
+                                                            citacoes = com_citacoes,
+                                                            saida_pdf = saida_pdf)
         except Exception as e:
-            html_ocr = controller.alerta(f'ERRO: {e}')
-            tipo_folha = ''
-        tempo = round((datetime.now()-_ini).total_seconds(),2)
-        #pedido_limpar_cache = dados.pop('limpar_cache','')
-        #print('HTML recebido: ', html_ocr[:100])
+            res = {'erro': f'ERRO: {e}' }
 
+        if res.get('erro'):
+            res['html'] = controller.alerta(res['erro'])
+        download_arquivo = None
+        if res.get('id'):
+           exemplo = ''
+           #res['html'] = f'Dados para download: {res}'
+           download_arquivo = res
+        tipo_folha = res.get('tipo_folha','')
+        tempo = round((datetime.now()-_ini).total_seconds(),2)
+        
+        # só informa um * para dizer que ignorou, mas não mantém ativo
+        ignorar_cache = '*' if ignorar_cache else ''
+        
         return render_template("aplicar_ocr_arquivo.html", 
                 titulo = titulo,
-                texto_ocr = Markup(str(html_ocr)),
+                texto_ocr = Markup(str(res.get('html',''))),
+                download_arquivo = download_arquivo,
                 tempo = f"{tempo:.3f} s",
                 ignorar_cache = ignorar_cache,
                 com_cabecalho = com_cabecalho,
                 com_estampas = com_estampas,
                 com_citacoes = com_citacoes,
+                saida_pdf = saida_pdf,
                 tipo_folha = tipo_folha,
+                displaychkhtml = 'display: none' if saida_pdf else '',
                 exemplos = get_lista_exemplos(exemplo))
     except TemplateNotFound as e:
         return render_template_string(f'Página não encontrada: {e.message}')
@@ -124,8 +161,18 @@ def get_lista_exemplos(exemplo):
 if __name__ == "__main__":
     # carrega as regras
     #carregar_exemplos()
-    print( '#########################################')
-    print( 'Iniciando o serviço')
-    print( '-----------------------------------------')
+    from util_processar_pasta import ProcessarOcrThread
+    print( '##############################################')
+    print( 'Iniciando o serviço de ocerização de pastas')
+    print( '-------------------------------------------------')
+    ProcessarOcrThread()
+    print( '-------------------------------------------------')
+    print( 'Iniciando o serviço Flask')
+    print( '-------------------------------------------------')
     app.run(host='0.0.0.0', debug=False,port=8000)
+    print( '-------------------------------------------------')
+    print( 'Finalizando o serviço de ocerização de pastas')
+    print( '-------------------------------------------------')
+
+    ProcessarOcrThread.finalizar()
 
